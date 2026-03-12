e21d37711c0ef1a26bdd2bf5c8bade49b0559c68
amaankhan6911-art <amaankhan6911@gmail.com>
2026-03-12 13:06:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,21 +6,32 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const PORT = process.env.PORT || 3000;
+
+// Put your API key directly here
 const elevenlabs = new ElevenLabsClient({
   apiKey: "d57ed36c45110623212ad8e71de28f8790b76f2f9b362f03bf4bd65cf66da1de"
 });
 
-// Agent routing table
+// Agent routing
 const AGENT_IDS = {
   "1": "agent_9501khzsgfhtfqdb4hfmxsaxnq9k",
   "2": "agent_3601khzhq94re87848e4p0j7p3b9"
 };
 
+// Test route
+app.get("/", (req, res) => {
+  res.send("Korvage IVR server running");
+});
+
+// IVR handler
 app.post("/ivr-handler", async (req, res) => {
 
   const digitPressed = req.body.Digits;
   const fromNumber = req.body.From;
   const toNumber = req.body.To;
+
+  console.log("Digit pressed:", digitPressed);
 
   const agentId = AGENT_IDS[digitPressed];
 
@@ -34,10 +45,10 @@ app.post("/ivr-handler", async (req, res) => {
 
   try {
 
-    const twiml = await elevenlabs.conversational_ai.registerCall({
-      agent_id: agentId,
-      from_number: fromNumber,
-      to_number: toNumber
+    const twiml = await elevenlabs.conversationalAi.registerCall({
+      agentId: agentId,
+      fromNumber: fromNumber,
+      toNumber: toNumber
     });
 
     res.set("Content-Type", "text/xml");
@@ -45,9 +56,11 @@ app.post("/ivr-handler", async (req, res) => {
 
   } catch (error) {
 
+    console.error(error);
+
     res.send(`
 <Response>
-<Say>Sorry, something went wrong.</Say>
+<Say>Sorry, something went wrong connecting the assistant.</Say>
 </Response>
 `);
 
@@ -55,6 +68,6 @@ app.post("/ivr-handler", async (req, res) => {
 
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
